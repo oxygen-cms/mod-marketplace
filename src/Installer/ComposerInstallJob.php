385175a47d22786d08bb789928ee233d9eb3baed
Chris Chamberlain <c.chamberlain@iinet.net.au>
2014-12-08 12:21:15 +1100

    Added `COMPOSER_HOME` environment variable.

--- src/Installer/ComposerInstallJob.php
+++ src/Installer/ComposerInstallJob.php
@@ -77,6 +77,7 @@ class ComposerInstallJob {
         // Composer\Factory::getHomeDir() method
         // needs COMPOSER_HOME environment variable set
         putenv('COMPOSER=' . base_path() . '/composer.json');
+        putenv('COMPOSER_HOME=' . base_path());
 
         $log = $this->config->get('oxygen/marketplace::config.install.log');
         $progress = $this->config->get('oxygen/marketplace::config.install.progress');
