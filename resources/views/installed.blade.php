1d463fb11c31bf43f1692e039f824e37308478bd
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-04-08 16:28:08 +1000

    Changed layout of Installed page

--- resources/views/installed.blade.php
+++ resources/views/installed.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-<?php
+    <?php
 
     use Oxygen\Core\Html\Header\Header;
 
     $header = Header::fromBlueprint(
-        $blueprint,
-        Lang::get('oxygen/marketplace::ui.installed.title')
+            $blueprint,
+            Lang::get('oxygen/marketplace::ui.installed.title')
     );
 
     $header->setBackLink(URL::route($blueprint->getRouteName('getHome')));
@@ -21,51 +21,51 @@
         $header->setSubtitle('(Only Oxygen)');
     }
 
-?>
+    ?>
 
-<div class="Block">
-    {{ $header->render()}}
-</div>
-
-<div class="Row--layout Row--alignTop">
-    @include('oxygen/marketplace::filters', ['exclude' => ['q']])
-    <div class="Cell-twoThirds Cell--last Block">
-        @if(empty($installed))
-            <div class="Row--visual">
-                <h2 class="heading-gamma">No results</h2>
-            </div>
-        @endif
-        @foreach($paginator as $package)
-           <?php
-               $header = Header::fromBlueprint($blueprint, $package->getPrettyName(), ['vendor' => $package->getSplitname()[0], 'package' => $package->getSplitname()[1]], Header::TYPE_SMALL, 'item');
-               if($package->getPrettyName() !== $package->getName()) {
-                   $header->setSubtitle($package->getName());
-               }
-               echo $header->render();
-
-               $providers = $package->getProviders()
-           ?>
-           @if(!empty($providers))
-               <div class="Row--visual">
-                   <div class="Text--indent">
-                       @foreach($providers as $provider)
-                           <?php
-                               $header = Header::fromBlueprint($blueprint, $provider['class'], ['provider' => $provider['class']], Header::TYPE_TINY, 'provider');
-                               $header->setSubtitle($provider['name']);
-                               echo $header->render();
-                           ?>
-                           <p>{{{ $provider['description'] }}}</p>
-                       @endforeach
-                   </div>
-               </div>
-           @endif
-        @endforeach
+    <div class="Block">
+        {{ $header->render()}}
     </div>
-</div>
-@if(!empty($installed))
-    <div class="Row">
-        {{ $paginator->links() }}
+
+    <div class="Row--layout Row--alignTop">
+        @include('oxygen/marketplace::filters', ['exclude' => ['q']])
+        <div class="Cell-twoThirds Cell--last Block">
+            @if(empty($installed))
+                <div class="Row--visual">
+                    <h2 class="heading-gamma">No results</h2>
+                </div>
+            @endif
+            @foreach($paginator as $package)
+                <?php
+                $header = Header::fromBlueprint($blueprint, $package->getPrettyName(), ['vendor' => $package->getSplitname()[0], 'package' => $package->getSplitname()[1]], Header::TYPE_SMALL, 'item');
+                if($package->getPrettyName() !== $package->getName()) {
+                    $header->setSubtitle($package->getName());
+                }
+                echo $header->render();
+
+                $providers = $package->getProviders()
+                ?>
+                @if(!empty($providers))
+                    <div class="Row--visual">
+                        <div class="Text--indent">
+                            @foreach($providers as $provider)
+                                <?php
+                                $header = Header::fromBlueprint($blueprint, $provider['name'], ['provider' => $provider['class']], Header::TYPE_TINY, 'provider');
+                                $header->setSubtitle($provider['class']);
+                                echo $header->render();
+                                ?>
+                                <p>{{{ $provider['description'] }}}</p>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+        </div>
     </div>
-@endif
+    @if(!empty($installed))
+        <div class="Row">
+            {{ $paginator->links() }}
+        </div>
+    @endif
 
 @stop
