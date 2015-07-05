c38aea00fd5b98a9a595a716d950931063a42caa
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-07-05 12:11:25 +1000

    Fixed indentation

--- resources/preferences/providers.php
+++ resources/preferences/providers.php
@@ -4,17 +4,18 @@ use Oxygen\Preferences\Loader\Database\PreferenceRepositoryInterface;
 use Oxygen\Preferences\Loader\DatabaseLoader;
 
 Preferences::register('providers', function($schema) {
-        $schema->setTitle('Service Providers');
-        $schema->setLoader(new DatabaseLoader(app(PreferenceRepositoryInterface::class), 'providers'));
+    $schema->setTitle('Service Providers');
+    $schema->setLoader(new DatabaseLoader(app(PreferenceRepositoryInterface::class), 'providers'));
 
-        $schema->makeFields([
-            '' => [
-                'Service Providers' => [
-                    [
-                        'name' => 'list',
-                        'label' => 'Extra Providers'
-                    ]
+    $schema->makeFields([
+        '' => [
+            'Service Providers' => [
+                [
+                    'name' => 'list',
+                    'label' => 'Extra Providers',
+                    'editable' => false
                 ]
             ]
-        ]);
-    });
+        ]
+    ]);
+});
